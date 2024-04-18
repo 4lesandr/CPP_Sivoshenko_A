355dcdced5d23d4aec7573fb0685b7b8c5b6bb35
4lesandr <138587010+4lesandr@users.noreply.github.com>
2024-04-18 14:22:27 +0300

    lab2.v3

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,9 +1,3 @@
-/*
-1.	Написать программу движения точки с расширяющимся и гаснущим следом. Управление движением точки осуществляется с помощью клавиш вправо, влево и вверх, вниз.
-2.	Написать программу движения отрезка с расширяющимся и гаснущим следом. Управление движением отрезка осуществляется с помощью клавиш вправо, влево и вверх, вниз.
-Все основные примитивы – позиция, скорость, отрезок должны использовать общий класс геометрический вектор. Операции с векторами реализовать с помощью операторов. 
-*/
-
 #include "vector.h"
 #include "point.h"
 #include "mote.h"
@@ -21,5 +15,4 @@ int main(){
     return 0;
 }
 
-//cd /Users/aleksandr/Documents/GitHub/CPP_Sivoshenko_A/lab2 && clang++ -std=c++17 main.cpp -o lab2.o && ./lab2.o
 //cd /Users/aleksandr/Documents/GitHub/CPP_Sivoshenko_A/lab2 && clang++ -std=c++17 vector.cpp point.cpp mote.cpp head.cpp scene.cpp main.cpp -o lab2.o && ./lab2.o
